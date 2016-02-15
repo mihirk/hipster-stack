3051529e800cdf2486343bdd75f1bddc2a4b6373
mmihirk <mihir.khatwani@gmail.com>
2016-02-15 18:21:32 +0000

    Init Scala

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject hipster-stack "0.1.0-SNAPSHOT"
-  :description "Backend for how Hipster is your Stack"
-  :url "git@github.com:mihirk/hipster-stack.git"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
